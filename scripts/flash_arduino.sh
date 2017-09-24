7ba19c574886dece1620f4dff061302b9baefc0d
Lars Wadefalk <lars@wadefalk.se>
2017-09-24 16:26:20 +0200

    Fixed the flasher script with more useful paths and variables.

--- scripts/flash_arduino.sh
+++ scripts/flash_arduino.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-/home/lars/arduino-1.6.3/hardware/tools/avr/bin/avrdude -C/home/lars/arduino-1.6.3/hardware/tools/avr/etc/avrdude.conf -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:obj-m2560-arduino_mega2560/sd2iec.hex:i
+
+AVRDUDE=`which avrdude`
+# An alternate path to the avrdude executable could be something on the line below if you have the arduino environment installed somewhere.
+#/home/$USER/arduino-1.6.3/hardware/tools/avr/bin/avrdude
+
+CONFPATH=/etc/avrdude.conf
+# An alternate path to the conf file could be something on the line below if you have the arduino environment installed somewhere.
+#/home/$USER/arduino-1.6.3/hardware/tools/avr/etc/avrdude.conf
+
+PORT=ttyACM0
+HEXPATH=obj-m2560-arduino_mega2560/sd2iec.hex
+
+$AVRDUDE -C $CONFPATH -patmega2560 -cwiring -P/dev/$PORT -b115200 -D -Uflash:w:$HEXPATH:i
 
